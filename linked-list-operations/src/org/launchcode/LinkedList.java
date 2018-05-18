5b13855e3be0c6f830c2845424a8b43d7aca00ea
Kaustubh H Narasimhan <hnkaustubh@gmail.com>
2018-05-18 11:12:11 +0530

    Delete First, Last,nth from beginning,nth from last and middle elements

--- linked-list-operations/src/org/launchcode/LinkedList.java
+++ linked-list-operations/src/org/launchcode/LinkedList.java
@@ -45,5 +45,60 @@ public class LinkedList {
         prev.next = n.next;
     }
 
-    
+    public void deleteNthToLastElement(int n) {
+        Node n1 = head;
+        Node n2 = head;
+        Node prev = null;
+
+        for (int i = 1; i < n; i++)
+            n2 = n2.next;
+
+        while (n2.next != null) {
+            prev = n1;
+            n1 = n1.next;
+            n2 = n2.next;
+        }
+        prev.next = n1.next;
+    }
+
+    public void deleteLastElement() {
+        Node n = head;
+        Node prev = null;
+
+        while (n.next != null) {
+            prev = n;
+            n = n.next;
+        }
+        prev.next = n.next;
+    }
+
+    public void deleteFirstElement() {
+        head = head.next;
+    }
+
+    public void deleteMiddleElement() {
+        Node n1 = head;
+        Node n2 = head;
+        Node prev = null;
+
+        while (n2.next != null) {
+            prev = n1;
+            n1 = n1.next;
+            n2 = n2.next.next;
+        }
+        prev.next = n1.next;
+    }
+
+    public void deleteNthFromBeginning(int n) {
+        Node p = head;
+        Node prev = null;
+        int count = 1;
+
+        while (p.next != null && count != n) {
+            count++;
+            prev = p;
+            p = p.next;
+        }
+        prev.next = p.next;
+    }
 }
