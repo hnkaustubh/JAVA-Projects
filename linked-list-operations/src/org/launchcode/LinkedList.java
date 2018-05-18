c390f8b0a7800e05c3166f2c9a6573e9ec6c4a59
Kaustubh H Narasimhan <hnkaustubh@gmail.com>
2018-05-18 11:18:02 +0530

    Length of a Linked List, Partition a Linked List and Exchange the Positions of two Nodes

--- linked-list-operations/src/org/launchcode/LinkedList.java
+++ linked-list-operations/src/org/launchcode/LinkedList.java
@@ -215,4 +215,87 @@ public class LinkedList {
             prev.next = null;
         }
     }
+
+    public int length() {
+        Node n = head;
+        int count = 0;
+
+        while (n != null) {
+            count++;
+            n = n.next;
+        }
+        return count;
+    }
+
+    public void partition(int x) {
+        Node node = new Node();
+        node.data = x;
+        Node n = head;
+        Node prev = null;
+        LinkedList greaterThanX = new LinkedList();
+
+        while(n != null){
+            if(n.data > x){
+                greaterThanX.insert(n.data);
+                prev.next = n.next;
+                n = n.next;
+            }
+            else if(n.data < x){
+                prev = n;
+                n = n.next;
+            }
+            else if(n.data == x){
+                prev.next = n.next;
+                n = n.next;
+            }
+        }
+
+        n = head;
+        while(n.next != null){
+            n = n.next;
+        }
+        n.next = node;
+        node.next = greaterThanX.head;
+    }
+
+    public void exchangePositions(int pos1,int pos2){
+        Node n1 = head;
+        Node n2 = head;
+        Node prev1 = null;
+        Node prev2 = null;
+        int count1 = 1,count2 = 1;
+
+        if(pos1 == 1){
+            while(n2.next != null && count2 != pos2){
+                count2++;
+                prev2 = n2;
+                n2 = n2.next;
+            }
+
+            Node next2 = n2.next;
+            head = n2;
+            n2.next = n1.next;
+            prev2.next = n1;
+            n1.next = next2;
+        }
+        else{
+            while(n1.next != null && count1 != pos1){
+                count1++;
+                prev1 = n1;
+                n1 = n1.next;
+            }
+
+            while(n2.next != null && count2 != pos2){
+                count2++;
+                prev2 = n2;
+                n2 = n2.next;
+            }
+
+            Node next2 = n2.next;
+            prev1.next = n2;
+            n2.next = n1.next;
+            prev2.next = n1;
+            n1.next = next2;
+        }
+    }
 }
