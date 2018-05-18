b767693879b711f92158048f51b80832ae8bcb86
Kaustubh H Narasimhan <hnkaustubh@gmail.com>
2018-05-18 11:13:46 +0530

    Delete Duplicates with and without buffer, Delete Duplicate Numbers

--- linked-list-operations/src/org/launchcode/LinkedList.java
+++ linked-list-operations/src/org/launchcode/LinkedList.java
@@ -101,4 +101,75 @@ public class LinkedList {
         }
         prev.next = p.next;
     }
+
+    public void deleteDups(){
+        Node n = head;
+        Node prev = null;
+        Hashtable ht = new Hashtable();
+
+        while(n != null){
+            if(ht.containsKey(n.data)){
+                prev.next = n.next;
+                n = n.next;
+            }
+            else{
+                ht.put(n.data,true);
+                prev = n;
+                n = n.next;
+            }
+        }
+    }
+
+    public void deleteDupsWithoutBuffer(){
+        Node n1 = head;
+        Node n2 = head.next;
+        Node prev = head;
+
+        while(n1.next != null){
+            prev = n1;
+            n2 = n1.next;
+            while(n2 != null){
+                if(n1.data == n2.data){
+                    prev.next = n2.next;
+                    n2 = n2.next;
+                }
+                else{
+                    prev = n2;
+                    n2 = n2.next;
+                }
+            }
+            n1 = n1.next;
+        }
+    }
+
+    public void deleteDuplicateNumbers(){
+        Node n1 = head;
+        Node n2 = head;
+        Node prev1 = null;
+        Node prev2 = null;
+        Hashtable ht = new Hashtable();
+
+        while(n1 != null){
+            if(ht.containsKey(n1.data)){
+                prev1.next = n1.next;
+                while(n2 != null){
+                    if(n1.data == n2.data){
+                        prev2.next = n2.next;
+                        n2 = n2.next;
+                    }
+                    else{
+                        prev2 = n2;
+                        n2 = n2.next;
+                    }
+                }
+                n2 = head;
+                n1 = n1.next;
+            }
+            else{
+                ht.put(n1.data,true);
+                prev1 = n1;
+                n1 = n1.next;
+            }
+        }
+    }
 }
